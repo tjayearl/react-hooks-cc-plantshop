845c3c0994b063fe80ec8d6272062b903dac51e7
tjayearl <iamtjayearl@gmail.com>
2025-04-29 16:50:18 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css"; // Make sure you have a corresponding CSS file for basic styling
+import "./index.css";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
